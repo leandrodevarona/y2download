5374acd07898f039b4f77d69a442791488d0aafb
Carlos-Ortiz-Mestre-6602 <comestre66@gmail.com>
2024-12-12 10:47:28 -0500

    manejando error url invalida

--- app/utils/yt_dlp.py
+++ app/utils/yt_dlp.py
@@ -2,24 +2,28 @@ import yt_dlp as yt
 from app.utils.random_name import get_random_name
 
 def download(url):
-    name = get_random_name()
+    try:
+        name = get_random_name()
 
-    ydl_opts = {
-        # 'format': 'bestaudio/best',
-        'format': 'bestvideo/best',
-        'outtmpl': f'static/{name}.' + '%(ext)s',
-    }
+        ydl_opts = {
+            # 'format': 'bestaudio/best',
+            'format': 'bestvideo/best',
+            'outtmpl': f'static/{name}.' + '%(ext)s',
+        }
 
-    with yt.YoutubeDL(ydl_opts) as ydl:
-        info_dict = ydl.extract_info(url, download=False)
+        with yt.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(url, download=False)
 
-        if info_dict['extractor'] is not 'youtube':
-            return 'error_invalid_url'
-        
-        ydl.download([url])
+            if info_dict['extractor'] is not 'youtube':
+                return 'error_invalid_url'
+            
+            ydl.download([url])
 
-        ext = info_dict['ext']
+            ext = info_dict['ext']
 
-        file_path = f'static/{name}.{ext}'
+            file_path = f'static/{name}.{ext}'
 
-        return file_path
+            return file_path
+    except:
+        return 'error_invalid_url'
+  
