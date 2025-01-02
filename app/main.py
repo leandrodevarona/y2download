40cf5ee7cdc9a09feaf11149b5f89f6095552387
Carlos-Ortiz-Mestre-6602 <comestre66@gmail.com>
2025-01-02 08:46:47 -0500

    end point http://127.0.0.1:8000

--- app/main.py
+++ app/main.py
@@ -13,13 +13,13 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 # Configuración de CORS
-#app.add_middleware(
-#    CORSMiddleware,
-#    allow_origins=["*"],  # Permite solicitudes desde cualquier origen
-#    allow_credentials=True,
-#    allow_methods=["*"],  # Permite todos los métodos (GET, POST, PUT, DELETE, etc.)
-#    allow_headers=["*"],  # Permite todos los encabezados
-#)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite solicitudes desde cualquier origen
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Permite todos los encabezados
+)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
